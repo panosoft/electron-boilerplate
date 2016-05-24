8159cfe4938af6f5c0fe4a964d7d3769815d0793
Alexandre Gigliotti <alex@panosoft.com>
2016-05-24 10:43:14 -0700

    Add check for update button. Improve uncaught exception handling.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const initialize = () => {
   // Handle uncaught exceptions
-  process.on('uncaughtException', e => console.error(e));
+  const {dialog} = require('electron');
+  process.on('uncaughtException', error => {
+    dialog.showErrorBox('Uncaught Exception', `Error: ${error.message}`);
+    process.exit(1);
+  });
 
-  // Handle native crashing
-
+  // Handle Squirrel Windows startup events
   const squirrelEvent = process.platform === 'win32' ? process.argv[1] : null;
   if (squirrelEvent) {
-    // Handle Squirrel Windows startup event
     const handle = require('./squirrel-event-handler');
     handle(squirrelEvent);
   }
+  // Start application
   else {
-    // Start application
     const main = require('./main');
     main();
   }
 };
-
 initialize();
--- src/main.js
+++ src/main.js
@@ -6,13 +6,13 @@ let mainWindow;
 const createWindow = () => {
   mainWindow = new BrowserWindow({width: 1024, height: 800});
 	mainWindow.on('closed', () => mainWindow = null);
-  mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.webContents.openDevTools();
 };
 
 const main = () => {
 	app.on('ready', () => {
 		createWindow();
+    mainWindow.webContents.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.webContents.openDevTools();
     if (!isDev) updater.initialize(mainWindow);
 	});
 	app.on('window-all-closed', () => {
@@ -21,7 +21,6 @@ const main = () => {
 	app.on('activate', () => {
 	  if (mainWindow === null) createWindow();
 	});
-
 
 	// In this file you can include the rest of your app's specific main process
 	// code. You can also put them in separate files and require them here.
